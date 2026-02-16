d0388988e5d821b551db20093e37535985f4d33b
DTK-MrRaccoon <dtkservera5@gmail.com>
2026-02-16 18:03:36 +0100

    transformators can now get damaged by overloadig them but they can be fixed with 4 plates and 4 copper coils

--- src/main/java/com/hbm/blocks/network/TransformerMedium.java
+++ src/main/java/com/hbm/blocks/network/TransformerMedium.java
@@ -1,10 +1,22 @@
 package com.hbm.blocks.network;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.hbm.blocks.BlockDummyable;
+import com.hbm.blocks.ILookOverlay;
 import com.hbm.blocks.ITooltipProvider;
+import com.hbm.inventory.RecipesCommon.AStack;
+import com.hbm.tileentity.IRepairable;
 import com.hbm.tileentity.TileEntityProxyConductor;
 import com.hbm.tileentity.network.TileEntityPylonBase;
 import com.hbm.tileentity.network.TileEntityTransformerMedium;
+import com.hbm.util.BobMathUtil;
+import com.hbm.util.i18n.I18nUtil;
+
+import api.hbm.block.IToolable;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.player.EntityPlayer;
@@ -12,9 +24,9 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.world.World;
-import java.util.List;
+import net.minecraftforge.client.event.RenderGameOverlayEvent.Pre;
 
-public class TransformerMedium extends BlockDummyable implements ITooltipProvider {
+public class TransformerMedium extends BlockDummyable implements ITooltipProvider, ILookOverlay, IToolable {
 
 	public TransformerMedium(Material mat) {
 		super(mat);
@@ -41,6 +53,8 @@ public class TransformerMedium extends BlockDummyable implements ITooltipProvide
 	public void addInformation(ItemStack stack, EntityPlayer player, List list, boolean ext) {
 		list.add(EnumChatFormatting.GOLD + "Connection Type: " + EnumChatFormatting.YELLOW + "Triple");
 		list.add(EnumChatFormatting.GOLD + "Connection Range: " + EnumChatFormatting.YELLOW + "35m");
+		list.add(EnumChatFormatting.GOLD + "Max Capacity: " + EnumChatFormatting.YELLOW + "2.50 MHE/t");
+		list.add(EnumChatFormatting.RED + "DANGER: Will melt if overloaded!");
 	}
 
 	@Override
@@ -57,14 +71,59 @@ public class TransformerMedium extends BlockDummyable implements ITooltipProvide
 		if(world.isRemote) {
 			return true;
 		} else if(!player.isSneaking()) {
+
 			int[] pos = this.findCore(world, x, y, z);
 			if(pos == null) return false;
-			
+
 			TileEntity te = world.getTileEntity(pos[0], pos[1], pos[2]);
+
 			if(te instanceof TileEntityPylonBase) {
 				return ((TileEntityPylonBase)te).setColor(player.getHeldItem());
 			}
 		}
 		return false;
+	}
+
+	@Override
+	public boolean onScrew(World world, EntityPlayer player, int x, int y, int z, int side, float fX, float fY, float fZ, ToolType tool) {
+		if(tool == ToolType.TORCH) {
+			return IRepairable.tryRepairMultiblock(world, x, y, z, this, player);
+		}
+		return false;
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void printHook(Pre event, World world, int x, int y, int z) {
+		int[] pos = this.findCore(world, x, y, z);
+		if(pos == null) return;
+		TileEntity te = world.getTileEntity(pos[0], pos[1], pos[2]);
+
+		if(te instanceof TileEntityTransformerMedium) {
+			TileEntityTransformerMedium pylon = (TileEntityTransformerMedium) te;
+
+			List<AStack> mats = IRepairable.getRepairMaterials(world, x, y, z, this, net.minecraft.client.Minecraft.getMinecraft().thePlayer);
+			
+			if (mats != null && pylon.isBroken) {
+				IRepairable.addGenericOverlay(event, world, x, y, z, this);
+				return;
+			}
+
+			List<String> text = new ArrayList<>();
+			text.add(EnumChatFormatting.GOLD + "Power Transfer: " + EnumChatFormatting.YELLOW + BobMathUtil.getShortNumber(pylon.powerTransfer) + " HE/t");
+			text.add(EnumChatFormatting.GOLD + "Max Transfer: " + EnumChatFormatting.RED + BobMathUtil.getShortNumber(TileEntityTransformerMedium.MAX_POWER) + " HE/t");
+
+			String tempColor = EnumChatFormatting.GREEN.toString();
+			if(pylon.temperature > 100) tempColor = EnumChatFormatting.YELLOW.toString();
+			if(pylon.temperature > 300) tempColor = EnumChatFormatting.RED.toString();
+
+			text.add(EnumChatFormatting.GOLD + "Temperature: " + tempColor + (int)pylon.temperature + " / " + (int)TileEntityTransformerMedium.MAX_TEMP + " °C");
+
+			if(pylon.isBroken) {
+				text.add(EnumChatFormatting.DARK_RED + "" + EnumChatFormatting.BOLD + "BROKEN - CRITICAL FAILURE");
+			}
+
+			ILookOverlay.printGeneric(event, I18nUtil.resolveKey(getUnlocalizedName() + ".name"), 0xffff00, 0x404000, text);
+		}
 	}
 }
--- src/main/java/com/hbm/tileentity/network/TileEntityTransformerMedium.java
+++ src/main/java/com/hbm/tileentity/network/TileEntityTransformerMedium.java
@@ -1,28 +1,185 @@
 package com.hbm.tileentity.network;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.hbm.inventory.RecipesCommon.AStack;
+import com.hbm.inventory.RecipesCommon.ComparableStack;
+import com.hbm.inventory.RecipesCommon.OreDictStack;
+import com.hbm.items.ModItems;
+import com.hbm.tileentity.IRepairable;
+import com.hbm.util.ParticleUtil;
 import com.hbm.util.fauxpointtwelve.BlockPos;
 import com.hbm.util.fauxpointtwelve.DirPos;
+
+import api.hbm.energymk2.Nodespace;
 import api.hbm.energymk2.Nodespace.PowerNode;
+import io.netty.buffer.ByteBuf;
+import net.minecraft.entity.Entity;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.Vec3;
+import net.minecraft.world.World;
 import net.minecraftforge.common.util.ForgeDirection;
 
-public class TileEntityTransformerMedium extends TileEntityPylonBase {
+public class TileEntityTransformerMedium extends TileEntityPylonBase implements IRepairable {
+
+	public float temperature = 20F;
+	public boolean isBroken = false;
+	public int burnTime = 0;
+	public int brokenTime = 0;
+	public long powerTransfer = 0;
+
+	public static final long MAX_POWER = 2_500_000L;
+	public static final float MAX_TEMP = 500F;
 
 	@Override
-	public ConnectionType getConnectionType() {
-		return ConnectionType.TRIPLE;
+	public void updateEntity() {
+		super.updateEntity();
+
+		if (!worldObj.isRemote) {
+			if (isBroken) {
+
+				powerTransfer = 0;
+
+				if(this.node != null) {
+					Nodespace.destroyNode(worldObj, xCoord, yCoord, zCoord);
+					this.node = null;
+				}
+
+				if (burnTime > 0) {
+					burnTime--;
+					if (worldObj.getTotalWorldTime() % 20 == 0) {
+						List<Entity> affected = worldObj.getEntitiesWithinAABB(Entity.class, AxisAlignedBB.getBoundingBox(xCoord - 1.5, yCoord, zCoord - 1.5, xCoord + 2.5, yCoord + 3.5, zCoord + 2.5));
+						for (Entity e : affected) {
+							e.setFire(5);
+						}
+					}
+				}
+				brokenTime++;
+
+			} else {
+				if (this.node != null && this.node.net != null) {
+					powerTransfer = this.node.net.energyTracker;
+				} else {
+					powerTransfer = 0;
+				}
+
+				if (powerTransfer > MAX_POWER) {
+					temperature += (powerTransfer - MAX_POWER) / (float)MAX_POWER / 25;
+					temperature -= 0.15F;
+				} else {
+					temperature -= 0.5F;
+				}
+
+				if (temperature < 20F) temperature = 20F;
+
+				if (temperature >= MAX_TEMP) {
+					isBroken = true;
+					burnTime = 2400;
+					brokenTime = 0;
+					temperature = MAX_TEMP;
+					worldObj.newExplosion(null, xCoord + 0.5, yCoord + 1.5, zCoord + 0.5, 2.5F, true, false);
+
+					if (this.node != null) {
+						Nodespace.destroyNode(worldObj, xCoord, yCoord, zCoord);
+						this.node = null;
+					}
+					this.markDirty();
+				}
+			}
+
+			if (worldObj.getTotalWorldTime() % 20 == 0) {
+				this.networkPackNT(50);
+			}
+
+		} else {
+			if (isBroken) {
+				if (burnTime > 0) {
+					ParticleUtil.spawnGasFlame(worldObj, xCoord + 0.5F + (worldObj.rand.nextFloat() - 0.5F), yCoord + 1.5F, zCoord + 0.5F + (worldObj.rand.nextFloat() - 0.5F), 0, 0.05, 0);
+				}
+				if (worldObj.rand.nextInt(4) == 0) {
+					worldObj.spawnParticle("largesmoke", xCoord + 0.5, yCoord + 1.5 + worldObj.rand.nextFloat(), zCoord + 0.5, 0, 0.1, 0);
+				}
+			} else {
+				if (temperature > 150F) {
+					if (worldObj.rand.nextInt(10) == 0) {
+						worldObj.spawnParticle("smoke", xCoord + 0.5 + (worldObj.rand.nextFloat() - 0.5F), yCoord + 2.0, zCoord + 0.5 + (worldObj.rand.nextFloat() - 0.5F), 0, 0.05, 0);
+					}
+				}
+			}
+		}
 	}
 
 	@Override
-	public boolean isUniversal() {
-		return true;
+	public boolean shouldCreateNode() {
+		return !this.isBroken;
 	}
 
 	@Override
-	public double getMaxWireLength() {
-		return 35D;
+	public PowerNode createNode() {
+		TileEntity tile = (TileEntity) this;
+		BlockPos corePos = new BlockPos(tile.xCoord, tile.yCoord, tile.zCoord);
+		PowerNode node = new PowerNode(corePos);
+
+		node.setConnections(new DirPos(xCoord, yCoord, zCoord, ForgeDirection.UNKNOWN));
+
+		for(int[] pos : this.connected) {
+			node.addConnection(new DirPos(pos[0], pos[1], pos[2], ForgeDirection.UNKNOWN));
+		}
+		for(ForgeDirection side : ForgeDirection.VALID_DIRECTIONS) {
+			if(side != ForgeDirection.UP && side != ForgeDirection.DOWN) {
+				node.addConnection(new DirPos(xCoord + side.offsetX, yCoord, zCoord + side.offsetZ, side));
+			}
+		}
+		return node;
 	}
+
+	@Override
+	public boolean isDamaged() {
+		return this.isBroken;
+	}
+
+	List<AStack> repair = new ArrayList<>();
+	@Override
+	public List<AStack> getRepairMaterials() {
+		if(!repair.isEmpty()) return repair;
+
+		repair.add(new OreDictStack("plateSteel", 4));
+		repair.add(new ComparableStack(ModItems.coil_copper, 4));
+		return repair;
+	}
+
+	@Override
+	public void repair() {
+		this.isBroken = false;
+		this.temperature = 20F;
+		this.burnTime = 0;
+		this.brokenTime = 0;
+
+		if (this.node != null) {
+			Nodespace.destroyNode(worldObj, xCoord, yCoord, zCoord);
+			this.node = null;
+		}
+
+		this.markDirty();
+	}
+
+	@Override
+	public void tryExtinguish(World world, int x, int y, int z, EnumExtinguishType type) {
+		if (this.isBroken && this.burnTime > 0) {
+			if (type == EnumExtinguishType.WATER || type == EnumExtinguishType.CO2 || type == EnumExtinguishType.FOAM) {
+				this.burnTime = 0;
+				this.markDirty();
+				this.networkPackNT(50);
+			}
+		}
+	}
+
+	@Override public ConnectionType getConnectionType() { return ConnectionType.TRIPLE; }
+	@Override public boolean isUniversal() { return true; }
+	@Override public double getMaxWireLength() { return 35D; }
 
 	@Override
 	public Vec3 getConnectionPoint() {
@@ -44,24 +201,40 @@ public class TileEntityTransformerMedium extends TileEntityPylonBase {
 	}
 
 	@Override
-	public PowerNode createNode() {
-		TileEntity tile = (TileEntity) this;
+	public void readFromNBT(NBTTagCompound nbt) {
+		super.readFromNBT(nbt);
+		temperature = nbt.getFloat("temperature");
+		isBroken = nbt.getBoolean("isBroken");
+		burnTime = nbt.getInteger("burnTime");
+		brokenTime = nbt.getInteger("brokenTime");
+	}
 
-		BlockPos corePos = new BlockPos(tile.xCoord, tile.yCoord, tile.zCoord);
-		PowerNode node = new PowerNode(corePos);
+	@Override
+	public void writeToNBT(NBTTagCompound nbt) {
+		super.writeToNBT(nbt);
+		nbt.setFloat("temperature", temperature);
+		nbt.setBoolean("isBroken", isBroken);
+		nbt.setInteger("burnTime", burnTime);
+		nbt.setInteger("brokenTime", brokenTime);
+	}
 
-		node.setConnections(new DirPos(xCoord, yCoord, zCoord, ForgeDirection.UNKNOWN));
+	@Override
+	public void serialize(ByteBuf buf) {
+		super.serialize(buf);
+		buf.writeFloat(temperature);
+		buf.writeBoolean(isBroken);
+		buf.writeInt(burnTime);
+		buf.writeInt(brokenTime);
+		buf.writeLong(powerTransfer);
+	}
 
-		for(int[] pos : this.connected) {
-			node.addConnection(new DirPos(pos[0], pos[1], pos[2], ForgeDirection.UNKNOWN));
-		}
-
-		for(ForgeDirection side : ForgeDirection.VALID_DIRECTIONS) {
-			if(side != ForgeDirection.UP && side != ForgeDirection.DOWN) {
-				node.addConnection(new DirPos(xCoord + side.offsetX, yCoord, zCoord + side.offsetZ, side));
-			}
-		}
-
-		return node;
+	@Override
+	public void deserialize(ByteBuf buf) {
+		super.deserialize(buf);
+		temperature = buf.readFloat();
+		isBroken = buf.readBoolean();
+		burnTime = buf.readInt();
+		brokenTime = buf.readInt();
+		powerTransfer = buf.readLong();
 	}
 }
